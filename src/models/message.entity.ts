0a3e04cda9a8ddcf11c95ef23c3c9f93d1d8e90c
diegomagalhaes-dev <digengup@gmail.com>
2022-02-20 22:46:13 -0300

    :zap: create user and message entities

---
+++ src/models/message.entity.ts
@@ -0,0 +1,39 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
+
+import User from './user.entity';
+
+@ObjectType()
+@Entity({ name: 'messages' })
+export default class Message {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column({ name: 'user_id' })
+  userId: number;
+
+  @Field()
+  @CreateDateColumn({ name: 'created_at' })
+  createAt: Date;
+
+  @Field()
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @Field(() => User)
+  user: User;
+
+  @ManyToOne(() => User, (user) => user.messageConnection, { primary: true })
+  @JoinColumn({ name: 'user_id' })
+  userConnection: Promise<User>;
+}
